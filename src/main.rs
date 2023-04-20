370e4598831d7d5eea54ca0527c650de66718c1c
reinlmao <rekreingd@gmail.com>
2023-04-19 21:49:31 -0500

    Optimize, improve readability, and add comments
    Added a frametime label as well for crude optimization benchmarking at runtime.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ impl Cell {
         Cell { alive: false }
     }
 
+    /// Update Cell living state, setting it to `alive`
     pub fn update(&mut self, alive: bool) {
         self.alive = alive;
     }
@@ -27,6 +28,7 @@ fn window_conf() -> Conf {
         window_title: "cells".to_owned(),
         window_width: 800,
         window_height: 800,
+        window_resizable: false,
         ..Default::default()
     }
 }
@@ -37,6 +39,7 @@ async fn main() {
 
     let mut rng = thread_rng();
 
+    // Initialize the Cell vector
     const SIZE: Vec2 = Vec2 { x: 100., y: 100. };
     let mut cell_vec = Vec::with_capacity(SIZE.x as usize);
     for _x in 0..(SIZE.x as i32) {
@@ -52,15 +55,19 @@ async fn main() {
 
     let mut field = Field { cells: cell_vec };
 
-    // GameLoop
+    // Game Loop
     loop {
         clear_background(BLACK);
 
+        let frametime = std::time::SystemTime::now();
+
+        // Iterate through cells, tracking position
         let mut x_pos = 0;
-        let im_cells = field.cells.to_vec();
+        let immut_cells = field.cells.to_vec(); // Immutable copy of `field.cells`
         for arr in &mut field.cells {
             let mut y_pos = 0;
             for cell in arr {
+                // Count neighbors' living state
                 let mut alive_count: i8 = 0;
                 for x_n in -1..=1 as i32 {
                     for y_n in -1..=1 as i32 {
@@ -73,13 +80,14 @@ async fn main() {
                                 x: x_pos as f32,
                                 y: y_pos as f32,
                             })
-                            && im_cells[(x_pos + x_n) as usize][(y_pos + y_n) as usize].alive
+                            && immut_cells[(x_pos + x_n) as usize][(y_pos + y_n) as usize].alive
                         {
                             alive_count += 1;
                         }
                     }
                 }
 
+                // Apply 'Game of Life' rules
                 if cell.alive {
                     if alive_count > 1 && alive_count < 4 {
                         cell.update(true);
@@ -90,29 +98,43 @@ async fn main() {
                     cell.update(true);
                 }
 
-                let real_pos = Vec2 {
-                    x: (x_pos * 8 + 4) as f32,
-                    y: (y_pos * 8 + 4) as f32,
-                };
-                let mouse_pos = Vec2 {
-                    x: mouse_position().0,
-                    y: mouse_position().1,
-                };
-                if is_mouse_button_down(MouseButton::Left) && real_pos.distance(mouse_pos) < 32. {
-                    cell.update(rng.gen_bool(0.5));
+                // Check mouse distance to Cell and randomize alive state if near
+                if is_mouse_button_down(MouseButton::Left) {
+                    let real_pos = Vec2 {
+                        x: (x_pos * 8 + 4) as f32,
+                        y: (y_pos * 8 + 4) as f32,
+                    };
+                    let mouse_pos = Vec2 {
+                        x: mouse_position().0,
+                        y: mouse_position().1,
+                    };
+
+                    if real_pos.distance(mouse_pos) < 32. {
+                        cell.update(rng.gen_bool(0.5));
+                    }
                 }
 
-                draw_rectangle(
-                    x_pos as f32 * 8.,
-                    y_pos as f32 * 8.,
-                    8.,
-                    8.,
-                    if cell.alive { WHITE } else { BLACK },
-                );
+                // Render Cell if alive
+                if cell.alive {
+                    draw_rectangle(x_pos as f32 * 8., y_pos as f32 * 8., 8., 8., WHITE);
+                }
                 y_pos += 1;
             }
             x_pos += 1;
         }
+
+        // Un-comment next lines to slow the simulation
+        // let factor = 25.;
+        // std::thread::sleep(std::time::Duration::new(1, 0).mul_f32(1. / factor));
+
+        // Show frametime for optimization benchmark
+        draw_text(
+            format!("FrameTime: {}ms", frametime.elapsed().unwrap().as_millis()).as_str(), // Holy method chain, I love Rust
+            5.,
+            20.,
+            20.,
+            GREEN,
+        );
 
         next_frame().await;
     }
